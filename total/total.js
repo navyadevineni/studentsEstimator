f8d8a4903cc441bf2edee135c960fa9a1dab7f68
Navya <navyadevineni30@gmail.com>
2020-03-02 12:02:40 -0600

    committed changes

--- total/total.js
+++ total/total.js
@@ -1,10 +1,8 @@
-const total = (input1, input2) => { return input1 * input2 }
+const totalFunction = (input1, input2) => { return input1 * input2 }
 
 const total = new Vue({
   el: '#total',
   data: {
-    inp1: 'countOfCourses',
-    inp2: 'averageOfCourses',
     input1: 5,
     input2: 3
   },
@@ -12,7 +10,7 @@ const total = new Vue({
     result: function () {
       const i = parseInt(this.input1)
       const j = parseInt(this.input2)
-      return `${this.guest}, your total is ${total(i, j)}.`
+      return ` your total is ${totalFunction(i, j)}.`
     }
   }
 })
