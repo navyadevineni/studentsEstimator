6edc3cfeffa26a6ece68ee67d2d82d7698a1a480
Navya <navyadevineni30@gmail.com>
2020-03-02 12:26:53 -0600

    formatted document

--- total/total.js
+++ total/total.js
@@ -1,16 +1,16 @@
 const totalFunction = (input1, input2) => { return input1 * input2 }
 
 const total = new Vue({
-  el: '#total',
-  data: {
-    input1: 5,
-    input2: 3
-  },
-  computed: {
-    result: function () {
-      const i = parseInt(this.input1)
-      const j = parseInt(this.input2)
-      return ` your total is ${totalFunction(i, j)}.`
+    el: '#total',
+    data: {
+        input1: 5,
+        input2: 3
+    },
+    computed: {
+        result: function () {
+            const i = parseInt(this.input1)
+            const j = parseInt(this.input2)
+            return ` your total is ${totalFunction(i, j)}.`
+        }
     }
-  }
 })
